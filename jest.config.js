b3b0f9e532ed4f05ed199f7c650bfe28203b891a
Ilia Znamenskii <ilia.znamenskii@project-plato.com>
2022-04-28 19:01:43 +0200

    check unit tests coverage

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,22 @@ module.exports = {
     moduleNameMapper: {
         "^@/(.*)$": "<rootDir>/src/$1",
     },
+    collectCoverageFrom: [
+        "src/app/**/*.{ts,tsx}",
+        "src/features/**/*.{ts,tsx}",
+        "src/libs/**/*.{ts,tsx}",
+        "src/ui/**/*.{ts,tsx}",
+        "src/utils/**/*.{ts,tsx}",
+    ],
+    coveragePathIgnorePatterns: [".stories.tsx", ".styled.ts", ".spec.{ts,tsx}", ".test.{ts,tsx}"],
+    coverageReporters: ["lcov", "text-summary"],
+    coverageDirectory: "coverage",
+    coverageThreshold: {
+        global: {
+            statements: 37,
+            branches: 100,
+            functions: 100,
+            lines: 37,
+        },
+    },
 };
